e59c1d67bdea817f518a9aa4d6e0aca06824cb9f
Shubham Singh <shubhsingh01@gmail.com>
2020-12-09 23:58:56 +0530

    Removed comment

--- src/hooks/useBoop.js
+++ src/hooks/useBoop.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useSpring } from 'react-spring';
-// UPDATE this path to your copy of the hook!
-// Source here: https://joshwcomeau.com/snippets/react-hooks/use-prefers-reduced-motion
+
 // import usePrefersReducedMotion from '@hooks/use-prefers-reduced-motion.hook';
+
 function useBoop({
   x = 0,
   y = 0,
